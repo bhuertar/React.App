707ab2f6bad11eb1dd3913b5df1acc23f9f47006
BryanHuerta <bhuertar@DESKTOP-OUBEMJ8>
2024-03-25 20:23:05 -0500

    rendered puppies names

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
+
+  return (
+    <>
+    { 
+      puppies.map((puppy) => {
+        return <p>{puppy.name}</p>
+      })
+   }
+   </>
+  )
+
 }
 
 export default App
