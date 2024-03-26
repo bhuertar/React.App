fa768550c74048824b630a89390e7c1b71a947d3
BryanHuerta <bhuertar@DESKTOP-OUBEMJ8>
2024-03-25 21:31:27 -0500

    redenred puppies details

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,37 @@ import './App.css'
 import {puppyList} from './data.js'
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    
+  }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
-    <>
-    { 
-      puppies.map((puppy) => {
-        return <p>{puppy.name}</p>
-      })
-   }
-   </>
-  )
-
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+          
+        );
+      })}
+      <p>{ featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}</p>
+    </div>
+  );
 }
 
 export default App
